753d5fc90279806b826be0e69b881a7eadb3f574
Aaron Honculada <aahon@amazon.com>
2022-05-30 17:04:40 -0700

    Testing out better structure for commit messages.
    Body of commit should include:
    - issue to be fixed
    - solution
    - how it was tested

--- notion.js
+++ notion.js
@@ -3,100 +3,99 @@ const { Client } = require('@notionhq/client')
 const notion = new Client({ auth: process.env.NOTION_API_KEY })
 
 async function getTopics() {
-	const database = await notion.databases.retrieve({ 
-		database_id: process.env.NOTION_DATABASE_ID 
-	})
-	return notionPropertiesById(database.properties)[
-		process.env.NOTION_TOPICS_ID
-	].multi_select.options.map(option => {
-		return { id: option.id, name: option.name }
-	})
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID,
+  })
+  return notionPropertiesById(database.properties)[
+    process.env.NOTION_TOPICS_ID
+  ].multi_select.options.map((option) => {
+    return { id: option.id, name: option.name }
+  })
 }
 
 async function getStatuses() {
-	const database = await notion.databases.retrieve({
-		database_id: process.env.NOTION_DATABASE_ID
-	})
-	return notionPropertiesById(database.properties)[
-		process.env.NOTION_STATUS_ID
-	].select.options.map(option => {
-		return { id: option.id, name: option.name }
-	})
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID,
+  })
+  return notionPropertiesById(database.properties)[
+    process.env.NOTION_STATUS_ID
+  ].select.options.map((option) => {
+    return { id: option.id, name: option.name }
+  })
 }
 
 async function getDifficulties() {
-	const database = await notion.databases.retrieve({ 
-		database_id: process.env.NOTION_DATABASE_ID 
-	})
-	return notionPropertiesById(database.properties)[
-		process.env.NOTION_DIFFICULTY_ID
-	].select.options.map(option => {
-		return { id: option.id, name: option.name }
-	})
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID,
+  })
+  return notionPropertiesById(database.properties)[
+    process.env.NOTION_DIFFICULTY_ID
+  ].select.options.map((option) => {
+    return { id: option.id, name: option.name }
+  })
 }
 
 function notionPropertiesById(properties) {
-	return Object.values(properties).reduce((obj, property) => {
-		const { id, ...rest } = property	
-		return { ...obj, [id]: rest }
-	}, {})
-	
+  return Object.values(properties).reduce((obj, property) => {
+    const { id, ...rest } = property
+    return { ...obj, [id]: rest }
+  }, {})
 }
 
-function createCard({ title, status, topics, difficulty, rating, URL, notes}) {
-	notion.pages.create({
-		parent: {
-			database_id: process.env.NOTION_DATABASE_ID
-		},
-		properties: {
-			[process.env.NOTION_TITLE_ID]: {
-				title: [
-					{
-						type: "text",
-						text: {
-							content: title
-						},
-					},
-				],
-			},
-			[process.env.NOTION_STATUS_ID]: {
-				select: {
-					id: status
-				}
-			},
-			[process.env.NOTION_TOPICS_ID]: {
-				multi_select: topics
-			},
-			[process.env.NOTION_DIFFICULTY_ID]: {
-				select: {
-					id: difficulty 
-				}
-			},
-			[process.env.NOTION_RATING_ID]: {
-				number: rating
-			},
-			[process.env.NOTION_LINK_ID]: {
-				rich_text: [
-					{
-						type: "text",
-						text: {
-							content: URL,
-						},
-					},
-				],
-			},
-			[process.env.NOTION_NOTES_ID]: {
-				rich_text: [
-					{
-						type: "text",
-						text: {
-							content: notes,
-						},
-					},
-				],
-			},
-		},
-	})
+function createCard({ title, status, topics, difficulty, rating, URL, notes }) {
+  notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DATABASE_ID,
+    },
+    properties: {
+      [process.env.NOTION_TITLE_ID]: {
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_STATUS_ID]: {
+        select: {
+          id: status,
+        },
+      },
+      [process.env.NOTION_TOPICS_ID]: {
+        multi_select: topics,
+      },
+      [process.env.NOTION_DIFFICULTY_ID]: {
+        select: {
+          id: difficulty,
+        },
+      },
+      [process.env.NOTION_RATING_ID]: {
+        number: rating,
+      },
+      [process.env.NOTION_LINK_ID]: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: URL,
+            },
+          },
+        ],
+      },
+      [process.env.NOTION_NOTES_ID]: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: notes,
+            },
+          },
+        ],
+      },
+    },
+  })
 }
 
 //topics = [{
@@ -104,19 +103,19 @@ function createCard({ title, status, topics, difficulty, rating, URL, notes}) {
 //	{
 //	"name": "Matrix"
 //}]
-//createCard({ 
-//	title: "Leet Code Problem", 
+//createCard({
+//	title: "Leet Code Problem",
 //	status: "Confident",
 //	topics: topics,
 //	difficulty: "Easy",
-//	rating: 5, 
-//	URL: "you_are_doing_great.com", 
-//	notes: "Keep up the good work.", 
+//	rating: 5,
+//	URL: "you_are_doing_great.com",
+//	notes: "Keep up the good work.",
 //})
 
 module.exports = {
-	getTopics,
-	getStatuses,
-	getDifficulties,
-	createCard,
+  getTopics,
+  getStatuses,
+  getDifficulties,
+  createCard,
 }
--- server.js
+++ server.js
@@ -1,58 +1,68 @@
 require('dotenv').config()
 const express = require('express')
-const { 
-	getStatuses,
-	getTopics,
-	getDifficulties,
-	createCard,
+const {
+  getStatuses,
+  getTopics,
+  getDifficulties,
+  createCard,
 } = require('./notion')
 
 const app = express()
-app.set("views", "./views")
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 let statuses = []
-getStatuses().then(data => {
-	statuses = data
+getStatuses().then((data) => {
+  statuses = data
 })
+
 let topics = []
-getTopics().then(data => {
-	topics = data
+getTopics().then((data) => {
+  topics = data
 })
+
 let difficulties = []
-getDifficulties().then(data => {
-	difficulties = data
+getDifficulties().then((data) => {
+  difficulties = data
 })
+
 setInterval(async () => {
-	statuses = await getStatuses()
-	topics = await getTopics()
-	difficulties = await getDifficulties()
+  statuses = await getStatuses()
+  topics = await getTopics()
+  difficulties = await getDifficulties()
 }, 1000 * 60 * 60)
 
 app.get('/', (req, res) => {
-	res.render("index", { statuses, topics, difficulties })
+  res.render('index', { statuses, topics, difficulties })
 })
 
-app.post("/create-card", async (req, res) => {
-	const { title, statusIds, topicIds = [], difficultyIds, rating, link, notes } = req.body
+app.post('/create-card', async (req, res) => {
+  const {
+    title,
+    statusIds,
+    topicIds = [],
+    difficultyIds,
+    rating,
+    link,
+    notes,
+  } = req.body
 
-	await createCard({
-		title: title,
-		status: statusIds,
-		topics: (Array.isArray(topicIds) ? topicIds : [topicIds]).map(topicId => {
-			return { id: topicId }
-		}),
-		difficulty: difficultyIds,
-		rating: parseInt(rating),
-		URL: link,
-		notes: notes 
-	})
+  await createCard({
+    title: title,
+    status: statusIds,
+    topics: (Array.isArray(topicIds) ? topicIds : [topicIds]).map((topicId) => {
+      return { id: topicId }
+    }),
+    difficulty: difficultyIds,
+    rating: parseInt(rating),
+    URL: link,
+    notes: notes,
+  })
 
-	res.redirect("/")
+  res.redirect('/')
 })
-
 
 app.listen(process.env.PORT)
